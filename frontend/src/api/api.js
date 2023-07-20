d1abede6e2ff3fb26da74464d067ca21b659f07a
Faithful Eromosele <faithfuleromosele001@gmai.com>
2023-07-20 14:48:29 +0100

    Initial commit

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,4 +1,4 @@
 export const API = {
-    prodAPI: "https://finance-tracker-y1xe.onrender.com/", 
+    prodAPI: "https://finance-tracker-api-3sb0.onrender.com/", 
     testAPI: "http://localhost:4000/"
 }
