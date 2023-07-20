93fbb284f56c8fd834f9e826dd4feb6567cde7dc
Faithful Eromosele <faithfuleromosele001@gmai.com>
2023-07-20 21:51:25 +0100

    Initial commit

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,27 +1,19 @@
-const mongoose = require("mongoose");
-const jwt = require("jsonwebtoken");
 const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
 
 const createToken = (_id) => {
   return jwt.sign({ _id }, process.env.SECRET, { expiresIn: "3d" });
 };
 
+// login a user
 const loginUser = async (req, res) => {
-  const {email, password} = req.body
-  try{
-    const user = await User.login(email, password)
-    const token = createToken(user._id)
-    res.status(200).json({email, token})
-  }catch(error){
-    res.status(400).json({error: error.message})
-  }
-};
-
-const signupUser = async (req, res) => {
   const { email, password } = req.body;
+
   try {
-    const user = await User.signup(email, password);
-    const token = createToken(user._id)
+    const user = await User.login(email, password);
+
+    // create a token
+    const token = createToken(user._id);
 
     res.status(200).json({ email, token });
   } catch (error) {
@@ -29,4 +21,20 @@ const signupUser = async (req, res) => {
   }
 };
 
-module.exports = { loginUser, signupUser };
+// signup a user
+const signupUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.signup(email, password);
+
+    // create a token
+    const token = createToken(user._id);
+
+    res.status(200).json({ email, token });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+module.exports = { signupUser, loginUser };
